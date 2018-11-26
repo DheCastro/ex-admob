d2b01309feadac30115b804d100264b38eb1f720
Dhellano de Castro Oliveira <dhecastro90@gmail.com>
2018-11-26 01:08:46 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/dhecastro/exadmob/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.dhecastro.exadmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Dhellano de Castro
+ * @since 26/11/2018
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
