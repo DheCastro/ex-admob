e62a6d52b943d5550dd165840b69fb19756df443
Dhellano de Castro Oliveira <dhecastro90@gmail.com>
2018-11-26 01:41:56 -0300

    Montagem da tela com inclusão do componente do banner

--- app/src/main/java/br/com/dhecastro/exadmob/MainActivity.java
+++ app/src/main/java/br/com/dhecastro/exadmob/MainActivity.java
@@ -3,15 +3,27 @@ package br.com.dhecastro.exadmob;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 /**
  * @author Dhellano de Castro
  * @since 26/11/2018
  */
 public class MainActivity extends AppCompatActivity {
 
+    private AdView adView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicializando e fazendo request ao serviço do banner
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 }
